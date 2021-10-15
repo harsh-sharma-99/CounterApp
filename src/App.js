9d4a5dcda6eaf37058d34b650704658887f5af24
harsh-sharma-99 <sharmaharsh9944@gmail.com>
2021-10-15 15:23:39 +0530

    Button Adjusted

--- src/App.js
+++ src/App.js
@@ -19,11 +19,39 @@ function App() {
   };
 
   return (
+    // <div class="columns is-multiline is-mobile has-text-centered">
+    //   <div class="column is-one-quarter">
+    //     <code>is-one-quarter</code>
+    //   </div>
+    //   <div class="column is-one-quarter">
+    //     <code>is-one-quarter</code>
+    //   </div>
+    //   <div class="column is-one-quarter">
+    //     <code>is-one-quarter</code>
+    //   </div>
+    //   <div class="column is-one-quarter">
+    //     <code>is-one-quarter</code>
+    //   </div>
+    //   <div class="column is-half">
+    //     <code>is-half</code>
+    //   </div>
+    //   <div class="column is-one-quarter">
+    //     <code>is-one-quarter</code>
+    //   </div>
+    //   <div class="column is-one-quarter">
+    //     <code>is-one-quarter</code>
+    //   </div>
+    //   <div class="column is-one-quarter">
+    //     <code>is-one-quarter</code>
+    //   </div>
+    //   <div class="column">Auto</div>
+    // </div>
+
     <div className=" container  ">
       <div className="box title is-1 has-background-grey-light ">
-        <h1 className=" has-text-light">COUNTER</h1>
+        <h1 className="has-text-centered has-text-light">COUNTER</h1>
       </div>
-      <h1 className="box has-background-grey-light title is-1">{count}</h1>
+      <h1 className="box has-text-centered title is-1">{count}</h1>
       <br />
       <progress class="progress is-medium is-info" max="100">
         60%
@@ -31,17 +59,28 @@ function App() {
       <progress class="progress is-large is-danger" max="100">
         60%
       </progress>
-      <div className="box">
-        <button onClick={addHandler} className="button is-link is-rounded">
-          <FontAwesomeIcon icon={faPlus} />
-          <span> Increment </span>
-        </button>
-      </div>
-      <div className="box">
-        <button onClick={subHandler} className="button is-danger is-rounded">
-          <FontAwesomeIcon icon={faMinus} />
-          Decrement
-        </button>
+
+      <div class="columns is-multiline is-mobile has-text-centered">
+        <div class="column is-half">
+          <div className="box">
+            <button onClick={addHandler} className="button is-link is-rounded ">
+              <FontAwesomeIcon icon={faPlus} />
+              <span> Increment </span>
+            </button>
+          </div>
+        </div>
+
+        <div class="column is-half">
+          <div className="box">
+            <button
+              onClick={subHandler}
+              className="button is-danger is-rounded"
+            >
+              <FontAwesomeIcon icon={faMinus} />
+              Decrement
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
